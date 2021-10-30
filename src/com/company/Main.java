75bbaae49a402f95d41a552160a2abfc31a0052d
Emintah <90718401+Emintah@users.noreply.github.com>
2021-10-31 01:15:41 +0200

    pain program virker ikke

---
+++ src/com/company/Main.java
@@ -0,0 +1,215 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+                AdjacencyGraph myGraphs =new AdjacencyGraph();
+                Vertex A=new Vertex("Eskildstrup");
+                Vertex B=new Vertex("Haslev");
+                Vertex C=new Vertex("Holbæk");
+                Vertex D=new Vertex("Jærgerspris");
+                Vertex E=new Vertex("Kalundborg");
+                Vertex F=new Vertex("Korsør");
+                Vertex G=new Vertex("Køge");
+                Vertex H=new Vertex("Maribo");
+                Vertex I=new Vertex("Næstved");
+                Vertex J=new Vertex("Ringsted");
+                Vertex K=new Vertex("Nykøbing F");
+                Vertex L=new Vertex("Vordingborg");
+                Vertex M=new Vertex("Roskilde");
+                Vertex N=new Vertex("Slagelse");
+                Vertex O=new Vertex("Sorø");
+                Vertex P=new Vertex("Nakskov");
+                myGraphs.addVertex(A);
+                myGraphs.addVertex(B);
+                myGraphs.addVertex(C);
+                myGraphs.addVertex(D);
+                myGraphs.addVertex(E);
+                myGraphs.addVertex(F);
+                myGraphs.addVertex(G);
+                myGraphs.addVertex(H);
+                myGraphs.addVertex(I);
+                myGraphs.addVertex(J);
+                myGraphs.addVertex(K);
+                myGraphs.addVertex(L);
+                myGraphs.addVertex(M);
+                myGraphs.addVertex(N);
+                myGraphs.addVertex(O);
+                myGraphs.addVertex(P);
+                myGraphs.addEdge(A,H,28);
+                myGraphs.addEdge(A,K, 13);
+                myGraphs.addEdge(A,L, 25);
+                myGraphs.addEdge(B,F, 60);
+                myGraphs.addEdge(B,G, 24);
+                myGraphs.addEdge(B,I, 25);
+                myGraphs.addEdge(B,J,19);
+                myGraphs.addEdge(B,M,47);
+                myGraphs.addEdge(B,N,48);
+                myGraphs.addEdge(B,O,34);
+                myGraphs.addEdge(B,L,40);
+                myGraphs.addEdge(C,D,34);
+                myGraphs.addEdge(C,E,44);
+                myGraphs.addEdge(C,F,66);
+                myGraphs.addEdge(C,J,36);
+                myGraphs.addEdge(C,M,32);
+                myGraphs.addEdge(C,N,46);
+                myGraphs.addEdge(C,O,34);
+                myGraphs.addEdge(D,F,94);
+                myGraphs.addEdge(D,G,58);
+                myGraphs.addEdge(D,J,56);
+                myGraphs.addEdge(D,M,33);
+                myGraphs.addEdge(D,N,74);
+                myGraphs.addEdge(D,O,63);
+                myGraphs.addEdge(E,J,62);
+                myGraphs.addEdge(E,M,70);
+                myGraphs.addEdge(E,N,39);
+                myGraphs.addEdge(E,O,51);
+                myGraphs.addEdge(F,I,45);
+                myGraphs.addEdge(F,N,20);
+                myGraphs.addEdge(G,I,45);
+                myGraphs.addEdge(G,J,28);
+                myGraphs.addEdge(G,M,25);
+                myGraphs.addEdge(G,L,60);
+                myGraphs.addEdge(H,P,27);
+                myGraphs.addEdge(H,K,26);
+                myGraphs.addEdge(I,M,57);
+                myGraphs.addEdge(I,J,26);
+                myGraphs.addEdge(I,N,37);
+                myGraphs.addEdge(I,O,32);
+                myGraphs.addEdge(I,L,28);
+                myGraphs.addEdge(J,M,31);
+                myGraphs.addEdge(J,O,15);
+                myGraphs.addEdge(J,L,58);
+                myGraphs.addEdge(N,O,14);
+                myGraphs.addEdge(K,A,13);
+                myGraphs.addEdge(K,H,26);
+                myGraphs.addEdge(L,A,25);
+                myGraphs.addEdge(L,B,40);
+                myGraphs.addEdge(L,G,60);
+                myGraphs.addEdge(L,I,28);
+                myGraphs.addEdge(L,J,58);
+                myGraphs.addEdge(M,B,47);
+                myGraphs.addEdge(M,C,36);
+                myGraphs.addEdge(M,D,33);
+                myGraphs.addEdge(M,E,70);
+                myGraphs.addEdge(M,G,25);
+                myGraphs.addEdge(M,I,57);
+                myGraphs.addEdge(M,J,31);
+                myGraphs.addEdge(O,B,34);
+                myGraphs.addEdge(O,C,34);
+                myGraphs.addEdge(O,D,63);
+                myGraphs.addEdge(O,E,51);
+                myGraphs.addEdge(O,I,32);
+                myGraphs.addEdge(O,J,15);
+                myGraphs.addEdge(O,N,14);
+                myGraphs.addEdge(P,H,27);
+                myGraphs.PrintGraph();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
